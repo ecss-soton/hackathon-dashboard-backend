aa2f3046a9d1c5c183d4c4790c5bbc1cd596790e
Jinxuan Cui <i@cjxol.com>
2019-01-19 12:25:13 +0000

    Add admin

--- index.js
+++ index.js
@@ -2,10 +2,17 @@ var config = require('./config.js').config;
 
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http, { origins: '*:*'});
+var io = require('socket.io')(http);
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 io.on('connection', function(socket) {
   console.log(`${socket.id} connected`);
+  socket.on('disconnect', function() {
+    console.log(`${socket.id} disconnected`);
+  });
 
   // send initial info
   socket.emit('event', config.event);
@@ -14,9 +21,7 @@ io.on('connection', function(socket) {
     end: config.hacking_end_time
   });
 
-  socket.on('disconnect', function() {
-    console.log(`${socket.id} disconnected`);
-  });
+  // client
   socket.on('request event', function() {
     socket.emit('event', config.event);
     console.log(`event sent to ${socket.id}`);
@@ -28,8 +33,15 @@ io.on('connection', function(socket) {
     });
     console.log(`hacking time sent to ${socket.id}`);
   });
-
 })
+
+// admin
+app.post('/admin/event', function(req, res) {
+  console.log(`requested to set event to: ${req.body.event}`);
+  io.emit('event', req.body.event);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.send('ok');
+});
 
 let port = process.env.PORT;
 if (port == null || port === "") {
